319612dcef226520cffecd972214194c0f02ac35
FedericaFerletti <f.ferletti@presstoday.com>
2022-11-30 11:09:03 +0100

    -

--- vendor/butschster/dbml-parser/src/Ast/Ref/RightTableNode.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace Butschster\Dbml\Ast\Ref;
-
-class RightTableNode extends TableNode
-{
-}
--- vendor/phplrt/lexer-contracts/src/polyfill.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Phplrt\Contracts\Lexer {
-
-    if (!interface_exists(BufferInterface::class, false)) {
-        class_alias(\Phplrt\Buffer\BufferInterface::class, BufferInterface::class);
-    }
-
-}
